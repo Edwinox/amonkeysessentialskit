f2f7efc3c7f9149d7e94ba2a2a04b46c9e594d7c
Edwinox <edwinlindbla123@gmail.com>
2025-03-06 12:07:24 +0100

    Added some more modded mobs to the loot tables (variants & ventures, creeper overhaul)

--- src/main/java/net/edwinox/monkeykit/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/net/edwinox/monkeykit/datagen/ModGlobalLootModifierProvider.java
@@ -127,5 +127,125 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
                         new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("entities/cave_spider")).build(),
                         LootItemRandomChanceCondition.randomChance(0.5f).build()
                 }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_verdant",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("variantsandventures/entities/verdant")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_thicket",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("variantsandventures/entities/thicket")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_murk",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("variantsandventures/entities/murk")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_gelid",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("variantsandventures/entities/gelid")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_jungle_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/jungle_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_bamboo_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/bamboo_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_desert_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/desert_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_badlands_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/badlands_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_hills_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/hills_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_savannah_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/savannah_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_mushroom_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/mushroom_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_swamp_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/swamp_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_dripstone_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/dripstone_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_cave_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/cave_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_dark_oak_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/dark_oak_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_spruce_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/spruce_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_beach_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/beach_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_snowy_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/snowy_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_ocean_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/ocean_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
+
+        this.add("copper_krona_from_birch_creeper",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("creeperoverhaul/entities/birch_creeper")).build(),
+                        LootItemRandomChanceCondition.randomChance(0.5f).build()
+                }, ModItems.COPPERKRONA.get()));
     }
 }
